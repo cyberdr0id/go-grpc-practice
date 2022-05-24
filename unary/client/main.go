b3b5d6f48df2de6edf4ef47205540a94770a454e
cyberdr0id <gopstrops@gmail.com>
2022-05-24 19:12:15 +0300

    Added input from command line arguments

--- unary/client/main.go
+++ unary/client/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-
+	"strconv"
 
 	"github.com/cyberdr0id/go-grpc-practice/unary/proto"
 	"google.golang.org/grpc"
@@ -16,6 +16,10 @@ var (
 )
 
 func main() {
+	if len(os.Args) != 4 {
+		log.Fatalf("Parameters number must be equal to 3")
+	}
+
 	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to connect to server: %v\n", err)
@@ -24,7 +28,17 @@ func main() {
 
 	c := proto.NewUnaryServiceClient(conn)
 
-	res, err := doSum(c)
+	x, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatalf("Invalid input parameter: %v\n", err)
+	}
+
+	y, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		log.Fatalf("Invalid input parameter: %v\n", err)
+	}
+
+	res, err := doSum(c, int32(x), int32(y))
 	if err != nil {
 		log.Fatalf("Cannot find sum: %v\n", err)
 	}
